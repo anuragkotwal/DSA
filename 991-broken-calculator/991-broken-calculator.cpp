294ab9bdd16ac7ec88765ec9c96550b0079f3baf
Anurag Kotwal <54587057+anuragkotwal@users.noreply.github.com>
2022-03-23 12:34:56 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (30.21%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int count=0;
+        while(target>startValue){
+            if(target%2!=0)
+                target++;
+            else
+                target/=2;
+            count++;
+        }
+        count+=startValue-target;
+        return count;
+    }
+};
