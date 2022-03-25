e7e9d9679628a4ef5c2ee307cb97e7128d4c8df5
Anurag Kotwal <54587057+anuragkotwal@users.noreply.github.com>
2022-03-25 12:57:40 +0530

    Time: 7 ms (52.53%), Space: 7.8 MB (94.48%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,19 +1,32 @@
+
+
+
 class Solution {
 public:
+    static bool comp(vector<int>& a, vector<int>& b){
+        return (a[0]-a[1])<(b[0]-b[1]);         
+    }   
     int twoCitySchedCost(vector<vector<int>>& costs) {
         int n=costs.size();
-        vector<int>temp(n);
+        sort(costs.begin(),costs.end(),comp);
         int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=costs[i][0]+costs[i][1];
-            temp[i]=costs[i][0]-costs[i][1];
-        }
-        sort(temp.begin(),temp.end());
-        
         for(int i=0;i<n/2;i++)
-            sum+=temp[i];
+            sum+=costs[i][0];
         for(int i=n/2;i<n;i++)
-            sum-=temp[i];
-        return sum/2;
+            sum+=costs[i][1];
+        return sum;
+//         vector<int>temp(n);
+//         int sum=0;
+//         for(int i=0;i<n;i++){
+//             sum+=costs[i][0]+costs[i][1];
+//             temp[i]=costs[i][0]-costs[i][1];
+//         }
+//         sort(temp.begin(),temp.end());
+        
+//         for(int i=0;i<n/2;i++)
+//             sum+=temp[i];
+//         for(int i=n/2;i<n;i++)
+//             sum-=temp[i];
+//         return sum/2;
     }
 };
