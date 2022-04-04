a254eff4abc4df841495dca4f427246ff32a3a37
Anurag Kotwal <54587057+anuragkotwal@users.noreply.github.com>
2022-04-04 20:09:29 +0530

    Time: 20 ms (9.21%), Space: 14.8 MB (82.13%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,30 +11,41 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* res=NULL;
-        vector<int> arr;
-        while(l1!=NULL){
-            arr.push_back(l1->val);
-            l1=l1->next;
+        // ListNode* res=NULL;
+        // vector<int> arr;
+        // while(l1!=NULL){
+        //     arr.push_back(l1->val);
+        //     l1=l1->next;
+        // }
+        // while(l2!=NULL){
+        //     arr.push_back(l2->val);
+        //     l2=l2->next;
+        // }
+        // sort(arr.begin(),arr.end());
+        // for(auto i:arr)
+        //     cout<<i<<" ";
+        // for(int i=0;i<arr.size();i++){
+        //     ListNode* n= new ListNode(arr[i]);
+        //     if(res==NULL)
+        //         res=n;
+        //     else{
+        //         ListNode* nextNode = res;
+        //         while(nextNode->next!=NULL)
+        //             nextNode=nextNode->next;
+        //         nextNode->next=n;
+        //     }
+        // }
+        // return res;
+        if(!l1)
+            return l2;
+        if(!l2)
+            return l1;
+        if(l1->val<=l2->val){
+            l1->next=mergeTwoLists(l1->next,l2);
+            return l1;
+        }else{
+            l2->next=mergeTwoLists(l1,l2->next);
+            return l2;
         }
-        while(l2!=NULL){
-            arr.push_back(l2->val);
-            l2=l2->next;
-        }
-        sort(arr.begin(),arr.end());
-        for(auto i:arr)
-            cout<<i<<" ";
-        for(int i=0;i<arr.size();i++){
-            ListNode* n= new ListNode(arr[i]);
-            if(res==NULL)
-                res=n;
-            else{
-                ListNode* nextNode = res;
-                while(nextNode->next!=NULL)
-                    nextNode=nextNode->next;
-                nextNode->next=n;
-            }
-        }
-        return res;
     }
 };
