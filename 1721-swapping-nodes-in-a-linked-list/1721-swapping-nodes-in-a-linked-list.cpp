4a80f47b86440536060eeb6ccb85d035ec959557
Anurag Kotwal <54587057+anuragkotwal@users.noreply.github.com>
2022-04-04 12:53:27 +0530

    Time: 797 ms (68.02%), Space: 180.2 MB (15.97%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -23,18 +23,15 @@ public:
         //     curr2=curr2->next;
         // swap(curr->val,curr2->val);
         // return head;
-        ListNode *ptr1 = head, *ptr2 = head, *kth = NULL;
-        while (--k)
-            ptr1 = ptr1->next;
-        
-        kth = ptr1;
-        ptr1 = ptr1->next;
-        
-        while (ptr1) {
-            ptr1 = ptr1->next;
-            ptr2 = ptr2->next;
+        ListNode *n1 = nullptr, *n2 = nullptr;
+        for (auto p = head; p != nullptr; p = p->next) {
+            n2 = n2 == nullptr ? nullptr : n2->next;
+            if (--k == 0) {
+                n1 = p;
+                n2 = head;
+            }
         }
-        swap(ptr2->val, kth->val);
-        return head;
+        swap(n1->val, n2->val);
+        return head;   
     }
 };
