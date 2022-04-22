2aba43e92d215ca7ffc04ed1dab04d6b2432dc36
Anurag Kotwal <54587057+anuragkotwal@users.noreply.github.com>
2022-04-22 11:17:50 +0530

    Time: 3 ms (98.57%), Space: 13.5 MB (98.47%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int l=0,h=nums.size();
-        while(l<h){
-            int mid = l + (h-l)/2;
-            if(nums[mid]>=target)
-                h = mid;
-            else
-                l=mid+1;
-        }
-        int temp=l;
-        l=0,h=nums.size();
-        while(l<h){
-            int mid=l+(h-l)/2;
-            if(nums[mid]>target)
-                h=mid;
-            else
-                l=mid+1;
-        }
-        return temp==l?vector<int> {-1,-1}:vector<int> {temp,l-1};
+        // 1st Approach
+        // int l=0,h=nums.size();
+        // while(l<h){
+        //     int mid = l + (h-l)/2;
+        //     if(nums[mid]>=target)
+        //         h = mid;
+        //     else
+        //         l=mid+1;
+        // }
+        // int temp=l;
+        // l=0,h=nums.size();
+        // while(l<h){
+        //     int mid=l+(h-l)/2;
+        //     if(nums[mid]>target)
+        //         h=mid;
+        //     else
+        //         l=mid+1;
+        // }
+        // return temp==l?vector<int> {-1,-1}:vector<int> {temp,l-1};
+        // 2nd Approach
+        int lo = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        if (lo == nums.size() || nums[lo] != target)
+            return {-1, -1};
+        int hi = upper_bound(nums.begin(), nums.end(), target) - nums.begin() - 1;
+        return {lo, hi};
     }
 };
