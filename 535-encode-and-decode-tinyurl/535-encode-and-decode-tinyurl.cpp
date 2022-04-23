f756bd8d9e0e662ff4e40e03da8f997524e9f952
Anurag Kotwal <54587057+anuragkotwal@users.noreply.github.com>
2022-04-23 12:29:04 +0530

    Time: 8 ms (31.19%), Space: 7 MB (45.21%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
+    
+    map<string,string> mp;
+    int s=0;
+    
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        return longUrl;
+        string en="http://tinyurl.com/"+to_string(s);
+        s++;
+        mp[en]=longUrl;
+        return en;
+        // return longUrl;
+        
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        return mp[shortUrl];
+        // return shortUrl;
     }
 };
 
