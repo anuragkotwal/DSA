492f0500e1735bab69a62904786abcef594da297
Anurag Kotwal <54587057+anuragkotwal@users.noreply.github.com>
2022-05-03 13:01:32 +0530

    Time: 46 ms (42.79%), Space: 26.6 MB (68.90%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
+        //Approach 1
+        // if(is_sorted(nums.begin(),nums.end()))
+        //     return 0;
+        // vector<int> arr(nums);
+        // sort(arr.begin(),arr.end());
+        // int s=0,e=0;
+        // for(int i=0;i<nums.size();i++)
+        //     if(arr[i]!=nums[i]){
+        //         s=i;
+        //         break;
+        //     }
+        // for(int i=nums.size()-1;i>=0;i--)
+        //     if(nums[i]!=arr[i]){
+        //         e=i;
+        //         break;
+        //     }
+        // return e-s+1;
+        
+        //Approach 2 w/o using extra space
         if(is_sorted(nums.begin(),nums.end()))
             return 0;
-        vector<int> arr(nums);
-        sort(arr.begin(),arr.end());
-        int s=0,e=0;
-        for(int i=0;i<nums.size();i++)
-            if(arr[i]!=nums[i]){
-                s=i;
-                break;
-            }
-        for(int i=nums.size()-1;i>=0;i--)
-            if(nums[i]!=arr[i]){
-                e=i;
-                break;
-            }
+        int s=0,e=0,i=0,j=nums.size()-1;
+        int curr_min=INT_MIN,curr_max=INT_MAX;
+        while(j>=0){
+            if(nums[i]>=curr_min)curr_min=nums[i];
+            else e=i;
+            if(nums[j]<=curr_max)curr_max=nums[j];
+            else s=j;
+            i++;j--;
+        }
         return e-s+1;
     }
 };
