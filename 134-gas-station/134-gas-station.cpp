2fd6e81c67f1b1605d071dcdc8a7a831e9811b6c
Anurag Kotwal <54587057+anuragkotwal@users.noreply.github.com>
2022-07-29 13:30:29 +0530

    Time: 103 ms (75.53%), Space: 69.4 MB (89.22%) - LeetHub

---
+++ 134-gas-station/134-gas-station.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        //brute force approach TLE!!
+    //     for(int i=0;i<gas.size();i++){
+    //         int totalFuel=0,count=0,j=i;
+    //         while(count<gas.size()){
+    //             totalFuel+=(gas[j%gas.size()]-cost[j%gas.size()]);
+    //             if(totalFuel<0)
+    //                 break;
+    //             count++;
+    //             j++;
+    //         }
+    //         if(count==gas.size() || totalFuel>=0)
+    //             return i;
+    //     }
+    //     return -1;
+        int tank=0,totalFuel=0,ind=0;
+        for(int i=0;i<gas.size();i++){
+            int temp=gas[i]-cost[i];
+            tank+=temp;
+            if(tank<0){
+                ind=i+1;
+                tank=0;
+            }
+            totalFuel+=temp;
+        }
+        return totalFuel>=0?ind:-1;
+    }
+};
